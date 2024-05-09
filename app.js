06c3d62fafe64b2436b8e71caf6ec4e9edb11d89
MikeJ58 <jmic@deakin.edu.au>
2024-05-09 10:53:49 +1000

    Commit

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const http = require('http'); 
 const path = require('path');
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 // Controllers
 const userController = require('./controllers/userController');
 const logController = require('./controllers/logController');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/login'); // Redirect to the login page
+});
+
 
 // MongoDB connection
 mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const app = express();
-const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
